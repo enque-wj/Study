78322619c7e7a587a701a7493cd7807211e43e37
enque-wj <wjwjwjj123@gmail.com>
2026-07-10 00:14:35 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 5.14 MB -BaekjoonHub

---
+++ C/프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    answer = n/7;
+    if(n % 7 == 0)
+    return answer;
+    
+    else return answer + 1;
+}
