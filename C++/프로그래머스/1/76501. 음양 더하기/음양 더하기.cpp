588482de8b866bc69fc09c4c04827d02d6763d3b
enque-wj <wjwjwjj123@gmail.com>
2026-07-29 19:14:44 +0900

    [level 1] Title: 음양 더하기, Time: 0.01 ms, Memory: 5.07 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int sum = 0;
+  for(int i =0;i<signs.size();i++){
+      if(signs[i] == 1){sum += absolutes[i];}
+      else sum -= absolutes[i];
+  }
+    return sum;
+}
