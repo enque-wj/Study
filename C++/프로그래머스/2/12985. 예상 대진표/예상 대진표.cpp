531f30cfef580f082586ac52637d569a4b611d99
enque-wj <wjwjwjj123@gmail.com>
2026-08-12 22:19:51 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 5.2 MB -BaekjoonHub

---
+++ C++/프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int round = 0;
+while(a != b){
+    
+    a = (a+1)/2;
+    b = (b+1)/2;
+    round ++;
+    
+}
+    
+    return round;
+}
