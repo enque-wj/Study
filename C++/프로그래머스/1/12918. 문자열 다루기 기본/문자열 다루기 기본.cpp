de7bcc3628c1d7022e86dae3b670dbb0f86185ac
enque-wj <wjwjwjj123@gmail.com>
2026-08-16 20:49:40 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.02 ms, Memory: 4.76 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+    
+    if(s.size() != 4 && s.size() != 6){ 
+        return false;
+    }
+    
+    for(int i = 0;i<s.size();i++){
+        
+        if(s[i] >= 'a'&& s[i]<='z' || s[i] >= 'A' && s[i] <= 'Z'){
+            answer = false;
+        }
+    }
+    return answer;
+}
